4d697e0c8df06a5745a6010f3d83860c55177459
Utsab <utsab1adhikari@gmail.com>
2025-11-27 22:56:01 +0545

    STL: List: P05_PopBack&Front added

---
+++ STL/List/P05_PopBackFront.cpp
@@ -0,0 +1,26 @@
+// Poping Elements from Front and Back of a List
+
+#include<iostream>
+#include<list>
+
+
+using namespace std;
+
+list<string> cars = {"Volvo", "BMW", "Ford", "Mazda"}; 
+
+int main() {
+    cout<<"<--- List of Cars --->"<<endl;
+    for(string car: cars) {
+        cout<<car<<endl;
+    }
+
+    // adding elements to front and back
+    cars.pop_front();
+    cars.pop_back();
+    
+    cout<<"<--- Updated List of Cars --->"<<endl;
+    for(string car: cars) {
+        cout<<car<<endl;
+    }
+    return 0;
+}
